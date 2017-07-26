d3eafa297779674c42f311e9ba3f06905c95c8fd
Valera Kushnir <kashura@users.noreply.github.com>
2017-07-25 20:07:08 -0400

    adding close()

--- exercises/ex15.py
+++ exercises/ex15.py
@@ -7,11 +7,12 @@ script, filename = argv
 # assign opening a file name to txt.
 txt = open(filename)
 
+
 # prints a message about the file.
 print(f"Here's your file {filename}:")
 # prints out the text from the file.
 print(txt.read())
-
+txt.close()
 
 # prints the message to the user to type the filename.
 print("Type the filename again:")
@@ -19,5 +20,7 @@ print("Type the filename again:")
 file_again = input("> ")
 # assigns opening of the file_again variable to txt_again.
 txt_again = open(file_again)
+
 # prints out what is the in the file_again to the user.
 print(txt_again.read())
+txt_again.close()
