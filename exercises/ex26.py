6e6deca43fe530a093f7bb4f044aec5f1ea997fa
Valera Kushnir <kashura@users.noreply.github.com>
2017-08-01 14:28:26 -0400

    fixing the script

--- exercises/ex26.py
+++ exercises/ex26.py
@@ -1,29 +1,33 @@
+from sys import argv
+from os.path import exists
+
+
 print("How old are you?", end=' ')
 age = input()
 print("How tall are you?", end=' ')
-print("How much do you weigh?", end=' '
+height = input()
+print("How much do you weigh?", end=' ')
 weight = input()
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
 
 script, filename = argv
 
-txt = open(filenme)
+txt = open(filename)
 
 print("Here's your file {filename}:")
-print(tx.read())
+print(txt.read())
 
 print("Type the filename again:")
 file_again = input("> ")
 
 txt_again = open(file_again)
 
-print(txt_again_read())
+print(txt_again.read())
 
 
-print('Let's practice everything.')
-print('You\'d need to know \'bout escapes
-      with \\ that do \n newlines and \t tabs.')
+print("Let's practice everything.")
+print("You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.")
 
 poem = """
 \tThe lovely world
@@ -34,18 +38,18 @@ and requires an explanation
 \n\t\twhere there is none.
 """
 
-print("--------------)
+print("--------------")
 print(poem)
-print(--------------")
+print("--------------")
 
 
-five = 10 - 2 + 3 -
-print(f"This should be five: {five}"
+five = 10 - (2 + 3)
+print(f"This should be five: {five}")
 
-def secret_formula(started)
-    jelly_beans = started * 500
+def secret_formula(start_point):
+    jelly_beans = start_point * 500
     jars = jelly_beans / 1000
-    crates = jars  100
+    crates = jars * 100
     return jelly_beans, jars, crates
 
 
@@ -60,19 +64,19 @@ print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 start_point = start_point / 10
 
 print("We can also do that this way:")
-formula = secret_formula(startpoint)
+formula = secret_formula(start_point)
 # this is an easy way to apply a list to a format string
 print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
 
 
 
 people = 20
-cates = 30
+cats = 30
 dogs = 15
 
 
 if people < cats:
-    print "Too many cats! The world is doomed!"
+    print ("Too many cats! The world is doomed!")
 
 if people < cats:
     print("Not many cats! The world is saved!")
@@ -80,7 +84,7 @@ if people < cats:
 if people < dogs:
     print("The world is drooled on!")
 
-if people > dogs
+if people > dogs:
     print("The world is dry!")
 
 
@@ -89,9 +93,9 @@ dogs += 5
 if people >= dogs:
     print("People are greater than or equal to dogs.")
 
-if people <= dogs
-    print("People are less than or equal to dogs.)
+if people <= dogs:
+    print("People are less than or equal to dogs.")
 
 
-if people = dogs:
+if people == dogs:
     print("People are dogs.")
