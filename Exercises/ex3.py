40f59b0da571843ea7da5f1d7bbdaac23f338891
Valera Kushnir <kashura@users.noreply.github.com>
2017-08-10 14:21:43 -0400

    ex3

---
+++ Exercises/ex3.py
@@ -0,0 +1,31 @@
+'''
++ plus - does addition
+- minus - does subtraction
+/ slash - does division
+* asterisk - does multiplication
+% percent - modulo; remainder of
+< less-than - less than x
+> greater-than - greater than x
+<= less-than-equal - equal or less than x
+>= greater-than-equal - equal or greater than x
+'''
+# prints a string
+print('I will now count my chickens:')
+# prints the string and an float after it.
+print("Hens", 25.0 + 30.0 / 6.0)
+# prints the string and the integer after it.
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+# prints the string
+print("Now I will count the eggs:")
+# prints the float from the operation below; simple math.
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+# prints a string with the question
+print("Is it true that 3 + 2 < 5 - 7?")
+# prints out a boolean statement of False.
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+
+# adding bill
+bill = 64.44
+fee = 2.45
+print(bill + fee)
