c60b2c6042d1b86daf230fb7fb04d1f9c53381a1
Valera Kushnir <kashura@users.noreply.github.com>
2017-08-11 16:19:33 -0400

    ex7

---
+++ Exercises/ex7.py
@@ -0,0 +1,30 @@
+# printing a string
+print("Mary had a little lamb.")
+# printing a string with formated string of snow
+print("Its fleece was white as {}.".format('snow'))
+# printing a string
+print("And everywhere that Mary went.")
+#printing a string "." and multiplying it by 10
+print("." * 10) # what'd that do? it printed string '.' 10 times.
+# assigning strings to variables in line 10 through 21
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. Try removing it to see what happens.
+# concatenating strings. After end6 we are ending with an empty string.
+print(end1 + end2 + end3 + end4 + end5 + end6, end=" ")
+# concatenating strings and priting them in the same line as in line 25. If end=" " is removed then the new strings are printed on separate lines.
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
+# Study drills
+# 1. Ok
