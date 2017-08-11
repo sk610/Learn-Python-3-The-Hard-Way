db1f77879dd2008f06c5cfd2554a2dd84374ab8e
Valera Kushnir <kashura@users.noreply.github.com>
2017-08-11 16:04:20 -0400

    ex6

---
+++ Exercises/ex6.py
@@ -0,0 +1,32 @@
+# assinging 10 to a variable
+types_of_people = 10
+# assigning a f-string to variable x
+x = f"There are {types_of_people} types of people."
+# lines 6 and 7 assign strings to variables, and 8 assigns an f-string.
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
+# lines 10 and 11 print out values of variables.
+print(x)
+print(y)
+# printing out f-strings with variables formated in them.
+print(f"I said: {x}")
+print(f"I also said '{y}'")
+# defining variable as a boolean value.
+hilarious = False
+# definding variable as a string.
+joke_evaluation = "Isn't that joke so funny?! {}"
+# printing joke_evaluation variable formated with the value of hilarious variable.
+print(joke_evaluation.format(hilarious))
+# defining variables with strings
+w = "This is the left side of ..."
+e = "a string with a right side."
+# concatenating strings and printing the result.
+print(w + e)
+
+
+# Study drills
+# 1. Done.
+# 2. Twice in line 8; once in line 14;
+# 3. Liar liar pants on fire. There are only 3 places.
+# 4. It's known as concatenating strings. We can add strings together to make 1 longer string. 
