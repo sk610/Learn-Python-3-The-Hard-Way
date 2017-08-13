36f3089f3a52d14fbe3f06234380394887ac6919
Valera Kushnir <kashura@users.noreply.github.com>
2017-08-13 15:58:07 -0400

    ex12 + sd

---
+++ Exercises/ex12.py
@@ -0,0 +1,45 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+
+# Study Drills
+'''
+1.
+input(...)
+    input([prompt]) -> value
+
+    Equivalent to eval(raw_input(prompt)).
+
+2. Ok
+
+
+3. Python documentation module. Allows developers to access Python documentation through the terminal.
+
+4. Help on built-in module sys:
+
+NAME
+    sys
+
+FILE
+    (built-in)
+
+MODULE DOCS
+    http://docs.python.org/library/sys
+
+DESCRIPTION
+    This module provides access to some objects used or maintained by the
+    interpreter and to functions that interact strongly with the interpreter.
+
+    Dynamic objects:
+
+    argv -- command line arguments; argv[0] is the script pathname if known
+    path -- module search path; path[0] is the script directory, else ''
+    modules -- dictionary of loaded modules
+
+    displayhook -- called to show results in an interactive session
+    excepthook -- called to handle any uncaught exception other than SystemExit
+
+'''
