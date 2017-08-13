ca916391f23a6f15f583150ec57c7fa0fec90921
Valera Kushnir <kashura@users.noreply.github.com>
2017-08-12 21:04:54 -0400

    ex11 + study drills

---
+++ Exercises/ex11.py
@@ -0,0 +1,21 @@
+print("How old are you?", end = ' ')
+age = input()
+print("How tall are you?", end = ' ')
+height = input()
+print("How much do you weigh?", end = ' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+# Study drills
+
+# 1. Input allows us to ask a user for input that we can use later.
+# 2. OK.
+favorite = input('What\'s your favorite team? ')
+print("Mine is ", favorite, "!")
+
+# 3. Ok
+home = input('Where are you from?')
+here = input('When did you move here?')
+state = input('What state do you live in?')
+print(f"You're from {home}, but moved here in {here} and live in {state}")
