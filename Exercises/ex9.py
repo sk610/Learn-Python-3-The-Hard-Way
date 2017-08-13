55409ff7477f67c6778669087978d640f2f9fb5f
Valera Kushnir <kashura@users.noreply.github.com>
2017-08-12 20:39:23 -0400

    study drills

--- Exercises/ex9.py
+++ Exercises/ex9.py
@@ -1,14 +1,24 @@
 # Here's some new strange stuff, remember to type it exactly.
-
+# defining a variable with a string of days.
 days = "Mon Tue Wed Thu Fri Sat Sun"
+# defining a variable with a string of months. \n indicates that what comes after should start on a new line.
 months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
-
+# printing variable days.
 print("Here are the days: ", days)
+# printing variable months.
 print("Here are the months: ", months)
-
+# printing text. 3 quotes allow us to type as much text as needed.
 print("""
 There's something going on here.
 With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
 """)
+
+
+# Study drills
+# 1. Ok.
+# 2. Ok.
+# 3. Ok.
+# 4. Ok.
+# 5. Ok. 
