0214c8dd1ba721a35fb1be7ac1b746d5a1bab2cd
Valera Kushnir <kashura@users.noreply.github.com>
2017-08-13 16:22:45 -0400

    ex13 study drill script

---
+++ Exercises/ex13sd.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, name, lastname = argv
+
+middlename = input("What's your middle name? ")
+
+print("Your first name is", name)
+print("Your last name is", lastname)
+print("And your middle name is", middlename)
