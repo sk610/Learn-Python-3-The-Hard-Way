1069c87f0a2233c6159ed48cb0f9a746921141d1
Valera Kushnir <kashura@users.noreply.github.com>
2017-08-19 16:24:36 -0400

    ex17 + sd

--- Exercises/ex17_sd.py
+++ Exercises/ex17_sd.py
@@ -9,8 +9,6 @@ print(f"Copying from {from_file} to {to_file}")
 in_file = open(from_file)
 indata = in_file.read()
 
-print(f"The input file is {len(indata)} bytes long")
-
 print("Ready, hit RETURN to continue, CTRL-C to abort.")
 input()
 
@@ -21,15 +19,3 @@ print("Alright, all done. Closing the files.")
 
 out_file.close()
 in_file.close()
-
-'''
-Study Drills
-
-1.Ok
-2. Couldn't figure it out at this time.
-3. Ok.
-4. Ok - preventing the system from wasting resources.
-5. Ok
-
-
-'''
