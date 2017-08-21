a521a737978b77f97a50bf96165969e5fa610737
Valera Kushnir <kashura@users.noreply.github.com>
2017-08-21 18:23:05 -0400

    ex19

---
+++ Exercises/ex19_sd.py
@@ -0,0 +1,12 @@
+def soccer_clubs (love, hate):
+    print(f"I absolutely love {love}!")
+    print(f"But I despise {hate}!")
+    print(" \n")
+
+soccer_clubs('Man United', 'Liverpool')
+
+soccer_clubs(input("What club do you hate?: "), input("What club do you love?: "))
+
+fav = 'Barcelona'
+least = 'Real Madrid'
+soccer_clubs(fav, least)
