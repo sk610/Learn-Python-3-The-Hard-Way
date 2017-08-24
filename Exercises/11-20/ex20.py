cd61a6fde27894995e64448f91c7a5ac1a8f7c24
Valera Kushnir <kashura@users.noreply.github.com>
2017-08-23 22:13:28 -0400

    working on the terminology here, still not there. A little confused.

--- Exercises/11-20/ex20.py
+++ Exercises/11-20/ex20.py
@@ -2,16 +2,17 @@
 from sys import argv
 # defining args
 script, input_file = argv
-# defining print_all funtion and saying that we need an f argument
+# defining print_all funtion and saying that we need an f argument (which is a file)
 def print_all(f):
     # printing the contents of the argument passed to the function.
     print(f.read())
 # defining rewind function with f argument.
 def rewind(f):
-    #
+    # 
     f.seek(0)
-
+# defining a function and required arguments.
 def print_a_line(line_count, f):
+    # printing
     print(line_count, f.readline())
 
 current_file = open(input_file)
