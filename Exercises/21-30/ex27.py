4791e15738c2f7ed9133ae1eb873eaf0c609eafa
Valera Kushnir <kashura@users.noreply.github.com>
2017-09-05 22:18:37 -0400

    tables

--- Exercises/21-30/ex27.py
+++ Exercises/21-30/ex27.py
@@ -12,4 +12,16 @@ False
 
 """
 Truth Tables
+
+
+NOT         True?
+not False   True
+not True    False
+
+
+OR          True?
+True or False   True
+True or True    True
+False or True   True
+False or False  False
 """
