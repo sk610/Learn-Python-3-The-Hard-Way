3abaa3df97c14dd3d6d60554bdc40e9e0166ce2b
Valera Kushnir <kashura@users.noreply.github.com>
2017-09-14 20:50:04 -0400

    ex30

--- Exercises/21-30/ex30.py
+++ Exercises/21-30/ex30.py
@@ -21,3 +21,23 @@ if people > trucks:
     print("Alright, let's just take the trucks.")
 else:
     print("Fine, let's stay home then.")
+
+
+if cars > people or trucks < cars:
+    print("Oh my, its true")
+elif cars < people or trucks > cars:
+    print("This is crazy")
+else:
+    print("high five!")
+
+"""
+Study Drills
+
+1. elif and else works like it would in normal speech. It gives the code multiple conditions, and then if it doesnt meet any of those the block of code is skipped.
+
+2. The answers will be different.
+
+3.Ok.
+
+4. 
+"""
