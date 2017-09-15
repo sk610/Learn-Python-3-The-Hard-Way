7407e9bb8dbd6f421645e7fbb322c4e0fc2747c8
Valera Kushnir <kashura@users.noreply.github.com>
2017-09-14 20:28:29 -0400

    ex29

--- Exercises/21-30/ex29.py
+++ Exercises/21-30/ex29.py
@@ -1,6 +1,6 @@
-people = 20
-cats = 30
-dogs = 15
+people = 30
+cats = 100
+dogs = 45
 
 
 if people < cats:
@@ -27,3 +27,24 @@ if people <= dogs:
 
 if people == dogs:
     print("People are dogs.")
+
+if True:
+    print("test")
+
+
+"""
+Study Drills
+
+
+1. What do you think the if does to the code under it?
+It basically tells Python to read what each variable means and act based on the response.
+
+2. To show that it belongs to that block of code.
+
+3. It won't work. IndentationError.
+
+4. Yes.
+
+5. The answers will change.
+
+"""
