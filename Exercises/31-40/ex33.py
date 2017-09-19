0d5f29d909581b9d91e86030aeff54b1c4865169
Valera Kushnir <kashura@users.noreply.github.com>
2017-09-18 21:55:57 -0400

    ex33

--- Exercises/31-40/ex33.py
+++ Exercises/31-40/ex33.py
@@ -1,15 +1,32 @@
-i = 0
-numbers = []
-
-while i < 6:
-    print(f"At the top i is {i}")
-    numbers.append(i)
-
-    i = i + 1
-    print("Numbers now: ", numbers)
-    print(f"At the bottom i is {i}")
 
 
-print("The numbers: ")
-for num in numbers:
-    print(num)
+def swoop(stuff, increment):
+
+	i = 0
+	numbers = []
+
+
+	while i < stuff:
+	    print(f"At the top i is {i}")
+	    numbers.append(i)
+
+	    i = i + increment
+	    print("Numbers now: ", numbers)
+	    print(f"At the bottom i is {i}")
+
+
+	print("The numbers: ")
+	for num in numbers:
+	    print(num)
+
+crazy = 15
+increment = 2
+super = swoop(crazy, increment)
+
+"""
+Study Drills
+
+1. Ok
+2. Ok
+3. 
+"""
