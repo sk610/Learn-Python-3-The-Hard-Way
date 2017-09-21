69e1b434eee3a2455a03d7d97b3bc0b4c965f52e
Valera Kushnir <kashura@users.noreply.github.com>
2017-09-20 20:28:45 -0400

    ex

---
+++ Exercises/31-40/functions.py
@@ -0,0 +1,16 @@
+# Trying to grasp functions better
+
+def example():
+    print('basic function')
+    z = 3 + 9
+    print(z)
+
+example()
+
+
+def simple_addition(num1, num2):
+    answer = num1 + num2
+    print('num1 is', num1)
+    print(answer)
+
+simple_addition(5, 3)
