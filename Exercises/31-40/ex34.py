959dbf5c0c9c5875c7a00e960f1e804a97ccf4f5
Valera Kushnir <kashura@users.noreply.github.com>
2017-09-20 20:35:41 -0400

    ex34

---
+++ Exercises/31-40/ex34.py
@@ -0,0 +1,12 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+"""
+1. The animal at 1 is python3.6
+2. The third animal is peacock.
+3. The first animal is bear.
+4. The animal at 3 is kangaroo.
+5. The fifth animal is platypus.
+6. The animal at 2 is peacock.
+7. The sisth animal is platypus.
+8. The animal at 4 is whale.
+"""
