d053cef0853cbf09290ac4da270ff5e52a7cdc59
Valera Kushnir <kashura@users.noreply.github.com>
2017-09-27 22:01:18 -0400

    ex

--- Exercises/31-40/ex36.py
+++ Exercises/31-40/ex36.py
@@ -6,5 +6,18 @@ Rules for if-statements
 3. Never nest if-statements more than two deep and always try to do them one deep.
 4. Treat if-statements like paragraphs, where each if-elif-else grouping is like a set of sentences. Put blank lines before and after. 
 5. Your Boolean tests should be simple. If they are complex, move their calculations to variables earlier in your function and use a good name for the variable. 
+
+
+Rules for Loops
+
+1. Use a while-loop only to loop forever, and that means probably never. This only applies to Python.
+2. Use a for-loop for all other kinds of looping, especially if there is a fixed or limited number of things to loop over. 
+
+
+Tips for Debugging
+
+1. Do not use a "debugger".
+2. The best way to debug a program is to use print to print out the values of variables at points in the program to see where they go wrong. 
+3. Make sure parts of your programs works as you work on them. Do not write massive files of code before you try to run them. Code a little, run a little, fix a little. 
 '''
 
