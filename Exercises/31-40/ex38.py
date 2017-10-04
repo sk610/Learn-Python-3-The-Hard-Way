8a819407397f161782bef78d82424e7de78b5fc2
Valera Kushnir <kashura@users.noreply.github.com>
2017-10-03 22:52:21 -0400

    ex38

--- Exercises/31-40/ex38.py
+++ Exercises/31-40/ex38.py
@@ -3,8 +3,8 @@ ten_things = "Apples Oranges Crows Telephone Light Sugar"
 print("Wait there are not 10 things in that list. Let's fix that.")
 
 stuff = ten_things.split(' ')
-print(stuff)
-'''
+#print(stuff)
+
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
 
@@ -23,4 +23,3 @@ print(stuff[-1])
 print(stuff.pop())
 print(' '.join(stuff))
 print('#'.join(stuff[3:5]))
-'''
