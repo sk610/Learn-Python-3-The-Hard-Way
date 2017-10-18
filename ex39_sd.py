c8196e17b3402d79dee072298563dbcb438b073d
kashura <kashura@users.noreply.github.com>
2017-10-17 21:41:08 -0400

    fixed euCities dict.

--- ex39_sd.py
+++ ex39_sd.py
@@ -5,10 +5,20 @@ euCountries = {
     'Denmark': 'DK',
     'Germany': 'DE'
 }
+
 # European capitals
-euCapitals = {
+euCities = {
     'BE': 'Brussels',
     'BG': 'Sofia',
     'DK': 'Copenhagen',
     'DE': 'Berlin'
 }
+
+# Adding more cities
+euCities['DE'] = 'Koln'
+euCities['DK'] = 'Kolding'
+
+# printing out cities
+print('-' * 10)
+print('Germany has: ', euCities['DE'])
+print('-' * 10)
