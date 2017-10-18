12153169df07faa4ce6837ed320d1fb9658e10cf
kashura <kashura@users.noreply.github.com>
2017-10-17 21:35:28 -0400

    Using dicts

---
+++ ex39_sd.py
@@ -0,0 +1,14 @@
+# European countries and their abbreviations
+euCountries = {
+    'Belgium': 'BE', 
+    'Bulgaria': 'BG',
+    'Denmark': 'DK',
+    'Germany': 'DE'
+}
+# European capitals
+euCapitals = {
+    'BE': 'Brussels',
+    'BG': 'Sofia',
+    'DK': 'Copenhagen',
+    'DE': 'Berlin'
+}
